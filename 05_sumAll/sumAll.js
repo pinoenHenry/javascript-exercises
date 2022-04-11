a4bb83d099157ac313fffdff07cbc30910020f53
Emilio Pino <pinoen2016@gmail.com>
2022-04-11 12:39:15 -0300

    1 to 7 javascript solutions

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
-};
+const sumAll = function(a,b) {
+    let sum = 0;
+    if(a < 0 || b < 0) return 'ERROR';
+    if(typeof a !== 'number' || typeof b !== 'number') return 'ERROR';
+    if(a < b){
+      for(let i=a; i<=b; i++){
+      sum += i;
+      } 
+    }
+    if(b < a){
+      for(let i=b; i<=a; i++){
+      sum += i;
+      }  
+    }
+    return sum;
+  };
 
 // Do not edit below this line
 module.exports = sumAll;
